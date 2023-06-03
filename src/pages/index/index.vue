25c015f344662e76294bf516ea3388ef8618c76d
chenqifeng66 <839860616@qq.com>
2023-06-03 18:22:10 +0800

    feat: 大致完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,48 +1,121 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <view class="container">
+    <view class="logo">Todo</view>
+    <view class="main">
+      <checkbox-group>
+        <view class="todo" v-for="(todo, index) in todoList" :key="todo.id">
+          <view class="left" :class="{ finished: todo.finished }">
+            {{ index + 1 }}.
+          </view>
+
+          <view class="middle">
+            <view class="content" :class="{ finished: todo.finished }">{{
+              todo.title
+            }}</view>
+            <checkbox v-model="todo.finished" @click="clickTodo(index)"
+          /></view>
+          <!-- <view class="right">
+          <button>完成</button>
+          <button>重做</button>
+        </view> -->
+        </view>
+      </checkbox-group>
+    </view>
+    <view class="add">
+      <button @click="add">添加</button>
     </view>
   </view>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
-}
+<script setup>
+import { uni } from "@dcloudio/uni-h5";
+import { onMounted, reactive } from "vue";
+
+const todoList = reactive([]);
+
+onMounted(() => {
+  const todos = uni.getStorageSync("todoList");
+  if (todos) todoList.push(...todos);
+});
+
+const add = () => {
+  uni.showModal({
+    title: "提示",
+    editable: true,
+    success: function (res) {
+      if (res.confirm) {
+        todoList.push({
+          id: todoList.length.toString(),
+          title: res.content,
+          finished: false,
+        });
+        save();
+      }
+    },
+  });
+};
+
+const clickTodo = (index) => {
+  const todo = todoList[index];
+  todoList.splice(index, 1);
+
+  if (todo.finished) {
+    todo.finished = false;
+    todoList.unshift(todo);
+  } else {
+    todo.finished = true;
+    todoList.push(todo);
+  }
+  save();
+};
+
+const save = () => {
+  uni.setStorageSync("todoList", todoList);
+};
 </script>
 
-<style>
-.content {
+<style lang="less">
+.container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin: 3rem 3rem;
+  padding: 1rem 0;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+
+  .logo {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  .main {
+    width: 100%;
+
+    .todo {
+      border-top: 1px solid #ccc;
+      display: flex;
+      padding: 1rem;
+
+      .left {
+        width: 2rem;
+      }
+
+      .middle {
+        display: flex;
+        flex: 1;
+        justify-content: space-between;
+      }
+      .right {
+        display: flex;
+      }
+    }
+  }
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.finished {
+  color: #ccc;
+  text-decoration: line-through;
 }
 </style>
